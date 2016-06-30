1b3ea599366fc2b541264af407513faa76c59775
Rioukay <kethewal@gmail.com>
2016-06-30 16:55:19 +0200

    Simple changes
    Simple changes in the generated files

--- rioko.specdrawing/dist/code/AuxiliaryGraphCreator_0.java
+++ rioko.specdrawing/dist/code/AuxiliaryGraphCreator_0.java
@@ -3,6 +3,6 @@ import rioko.grapht.linear.UndirectedGraph;
 
 public class AuxiliaryGraphCreator_0 implements UndirectedGraphCreator {
 	public UndirectedGraph create() {
-		return cycle(10);
+		return join(cycle(3), cycle(3), 0,0);
 	}
 }
